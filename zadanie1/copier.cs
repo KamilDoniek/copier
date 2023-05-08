2498b2573a18c3681a8d86c8c8e0fe73cbacb51e
KamilDoniek <kamil.doniek@microsoft.wsei.edu.pl>
2023-05-08 12:14:00 +0200

    commit

--- zadanie1/copier.cs
+++ zadanie1/copier.cs
@@ -5,7 +5,7 @@ using static zadanie1.IDevice;
 
 namespace zadanie1;
 
-public class Copier : IDevice , IPrinter , IScanner
+public class Copier :  IPrinter , IScanner
 {
     private int _PrintCounter;
     private int _ScanCounter;
@@ -14,63 +14,74 @@ public class Copier : IDevice , IPrinter , IScanner
     public int PrintCounter
     {
         get => _PrintCounter;
-        init =>_PrintCounter = value;
+        set =>_PrintCounter = value;
         
     }
 
     public int ScanCounter
     {
         get => _ScanCounter;
-        init => _ScanCounter = value;
+        set => _ScanCounter = value;
     }
 
     public int Counter
     {
         get => _Counter;
-        init => _Counter = value;
+        set => _Counter = value;
 
     }
 
     
 
-
+    private    IDevice.State state;
     public void PowerOn()
     {
-
+        if (state == IDevice.State.off)
+        {
+            state = IDevice.State.on;
+            Counter++;
+        }
+      
     }
 
    
 
     public void PowerOff()
     {
-        
+        state = IDevice.State.off;   
     }
 
     public State GetState()
     {
-        throw new NotImplementedException();
+       return  state;
     }
+    
+
     public void Print(in IDocument document)
     {
         DateTime thisDay = DateTime.Today;
         string data = $"{thisDay.ToString("d")}";
         string time = $"{thisDay.ToString("T")}";
+
+        if (GetState()==IDevice.State.off){ Console.WriteLine("The device is powered off"); return;}
         
         Console.WriteLine($"{data} {time} Print: {document.GetFileName()}.{document.GetFormatType()}");
+        PrintCounter++;
     }
-    public void Scan(out IDocument document,IDocument.FormatType formatType)
+    public void Scan(out IDocument document,IDocument.FormatType formatType= IDocument.FormatType.JPG)
     {
         DateTime thisDay = DateTime.Today;
         string data = $"{thisDay.ToString("d")}";
         string time = $"{thisDay.ToString("T")}";
         string name = "";
 
-     
-   
+
+       
         if (formatType == IDocument.FormatType.PDF)
         {
             name = $"PDFScan{Counter}.{formatType}";
             document = new PDFDocument(name);
+            
         }
         else if (formatType == IDocument.FormatType.JPG)
         {
@@ -87,20 +98,36 @@ public class Copier : IDevice , IPrinter , IScanner
             throw new ArgumentException("Unsupported document format type.");
         }
 
-        
         Console.WriteLine($"{data} {time} Scan: {name}" );
-       
+        ScanCounter++;
     }
+    // public void Scan(out IDocument document)
+    // {
+    //     DateTime thisDay = DateTime.Today;
+    //     string data = thisDay.ToString("d");
+    //     string time = thisDay.ToString("T");
+    //     string name = $"ImageScan{Counter}.JPG";
+    //
+    //     document = new ImageDocument(name);
+    //
+    //     Console.WriteLine($"{data} {time} Scan: {name}");
+    //     ScanCounter++;
+    // }
 
     public void ScanAndPrint()
     {
         IDocument document;
-        Scan(out document, IDocument.FormatType.JPG);
+        IDocument.FormatType formatType = IDocument.FormatType.JPG;
 
-        if (document != null) Print(document);
+        Scan(out document, formatType);
+
+        if (document != null) {Print(document); ScanCounter++;
+            PrintCounter++;}
 
         else throw new ArgumentException("Scanning failed");
+       
     }
 
+  
 }
 
