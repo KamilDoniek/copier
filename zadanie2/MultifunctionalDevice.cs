046ce52a4a2b2d3ef03e94037456b536d0fbb2c3
KamilDoniek <kamil.doniek@microsoft.wsei.edu.pl>
2023-05-24 13:24:25 +0200

    commit

--- zadanie2/MultifunctionalDevice.cs
+++ zadanie2/MultifunctionalDevice.cs
@@ -2,26 +2,11 @@ using zadanie1;
 
 namespace zadanie2;
 
-public class MultifunctionalDevice :BaseDevice, IPrinter, IScanner, IFax
+public class MultifunctionalDevice : Copier
 {
-    private int _printCounter=0;
-    private int _scanCounter=0;
-    private int _counter=0;
+  
     private int _faxCounter = 0;
     
-
-    public int PrintCounter
-    {
-        get => _printCounter;
-        private set =>_printCounter = value;
-        
-    }
-
-    public int ScanCounter
-    {
-        get => _scanCounter;
-        private set => _scanCounter = value;
-    }
 
     public int FaxCounter
     {
@@ -29,75 +14,6 @@ public class MultifunctionalDevice :BaseDevice, IPrinter, IScanner, IFax
         private set => _faxCounter = value;
     }
 
-    public new int Counter
-    {
-        get => _counter;
-        private set => _counter = value;
-
-    }
-    public void Print(in IDocument document)
-    {
-        DateTime thisDay = DateTime.Today;
-        string data = $"{thisDay.ToString("d")}";
-        string time = $"{thisDay.ToString("T")}";
-
-        if (GetState() == IDevice.State.on)
-        {
-            Console.WriteLine($"{data} {time} Print: {document.GetFileName()}");
-            PrintCounter++;
-        }
-    }
-    public void Scan(out IDocument document,IDocument.FormatType formatType= IDocument.FormatType.JPG)
-    {
-        DateTime thisDay = DateTime.Now;
-        string data = $"{thisDay.ToString("d")}";
-        string time = $"{thisDay.ToString("T")}";
-        string name = "";
-
-        document = null;
-        if (GetState()==IDevice.State.off){ 
-            document = null;
-            return;
-            
-        }
-     
-        if (formatType == IDocument.FormatType.PDF)
-        {
-            name = $"PDFScan{ScanCounter}.pdf";
-            document = new PDFDocument(name);
-
-        }
-        else if (formatType == IDocument.FormatType.JPG)
-        {
-            name = $"ImageScan{ScanCounter}.jpg";
-            document = new ImageDocument(name);
-        }
-        else if (formatType == IDocument.FormatType.TXT)
-        {
-            name = $"TextScan{ScanCounter}.txt";
-            document = new TextDocument(name);
-        }
-  
-        Console.WriteLine($"{data} {time} Scan: {name}");
-        ScanCounter++;
-
-    }
-    public void ScanAndPrint()
-    {
-        IDocument document;
-        IDocument.FormatType formatType = IDocument.FormatType.JPG;
-        
-        if (GetState()==IDevice.State.off){ 
-            Console.WriteLine("The device is powered off");
-            return;
-            
-        }
-        Scan(out document, formatType);
-        
-        Print(document);
-        
-            
-    }
 
     public void SendFax(in IDocument document)
     {
